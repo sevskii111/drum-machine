11725d7c327d458854ca1b8e5228514a45e329d4
sevskii <sevskii111@gmail.com>
2019-12-13 02:03:09 +0300

    init

---
+++ shared/config.js
@@ -0,0 +1,31 @@
+export const boards = {
+    "Analogue Chain": {
+        "Q": {
+            sound: "RDM_Analog_SY1-Kick01.wav"
+        },
+        "W": {
+            sound: "RDM_Analog_SY1-Kick02.wav"
+        },
+        "E": {
+            sound: "RDM_Analog_SY1-Kick03.wav"
+        },
+        "A": {
+            sound: "RDM_Analog_SY1-Perc01.wav"
+        },
+        "S": {
+            sound: "RDM_Analog_SY1-Perc02.wav"
+        },
+        "D": {
+            sound: "RDM_Analog_SY1-Perc03.wav"
+        },
+        "Z": {
+            sound: "RDM_Analog_SY1-Snr01.wav"
+        },
+        "X": {
+            sound: "RDM_Analog_SY1-Tom01.wav"
+        },
+        "C": {
+            sound: "RDM_Analog_SY1-OpHat.wav"
+        },
+    }
+};
